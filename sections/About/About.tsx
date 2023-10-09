14bae6ca8d095fa63a1af56a6b52ed1f4589b1fa
KORZH ILLIA <kiev_drum2006@ukr.net>
2023-10-09 21:37:16 +0300

    Update main page About section

--- sections/About/About.tsx
+++ sections/About/About.tsx
@@ -6,19 +6,23 @@ import SectionSubheader from "@/components/shared/SectionSubheader/SectionSubhea
 import Paragraph from "@/components/shared/Paragraph/Paragraph";
 
 import aboutImg from '@/public/assets/img/aboutImg.jpg';
+import about from '@/data/about.json';
 
 export default function About() {
     return (
         <Section styles="py-3" >
             <div className="flex flex-col lg:flex-row lg:items-center lg:justify-between lg:gap-x-3">
                 <div className="lg:w-[60%]">
-                    <SectionHeader text="Our Mission" isUnderline />
-                <Paragraph text="At Northern Heights Dental, people come first. We help each of our patients to achieve optimal wellness and health by using a whole body approach to oral health. This means not just focusing on cavities, but focusing on; cranio-facial development, bite and joint balance, oral flora, proper muscle balance/function, and bio-compatibility of dental materials. Great care and planning ensure that everything we do helps promote overall health and well being." />
-                <SectionSubheader text="More than anything else we love creating happy, healthy smiles." />
-                <Paragraph text='We work hard to stay up to date with the most advanced techniques and technologies to ensure that our patients receive the best care possible. Our office utilizes 3D CBCT radiographs to allow for guided surgical and endodontic protocols. This enables these procedures to performed digitally before they are performed surgically to ensure optimal results. 3D imaging also is utilized for the analysis of airway growth and development. We also use the best 3D optical scanner for all of our dental restoration and Invisalign impressions. Dr Williams is a strong advocate for using microsurgical techniques, this means less discomfort and faster healing times.' />
+                    <SectionHeader text={about.headerText} isUnderline />
+                    <Paragraph text={about.mainText1} />
+                    <h3 className="mb-4 text-header dark:text-slate-200 text-2xl">{about.mainText2_1}<span className="text-brand">* </span>{ about.mainText2_2}</h3>
+                    <Paragraph text={about.mainText3} />
+                    <Paragraph text={about.mainText4} />
+                    <SectionSubheader text={about.mainText5} />
+                    <p className="mb-4 lg:mb-0"><span className="text-brand">* </span>{ about.notaText}</p>
                 </div>
                 <div className="relative w-full lg:w-[35%] h-[517px] lg:h-[600px] rounded-lg overflow-hidden">
-                <Image className="object-cover" src={aboutImg} fill sizes='(max-width: 480px) 100vw, (max-width: 767px) 57vw, (max-width: 1000px) 71vw, 31vw' priority alt='' />
+                  <Image className="object-cover object-[35%_2px] sm:max-lg:object-[27%_2px]" src={aboutImg} fill sizes='(max-width: 480px) 100vw, (max-width: 767px) 57vw, (max-width: 1000px) 71vw, 31vw' priority alt='' />
                 </div>
             </div>
         </Section>
